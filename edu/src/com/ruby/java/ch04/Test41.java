ada005bef18d1f15a2a2c5614b960f61598ab7ed
DoyoungKim273 <160606319+DoyoungKim273@users.noreply.github.com>
2024-03-09 17:43:55 +0900

    Delete edu directory

--- edu/src/com/ruby/java/ch04/Test41.java
+++
@@ -1,9 +0,0 @@
-package com.ruby.java.ch04;
-
-public class Test41 {
-	public static void main(String[] args) {
-		for(int i = 0; i < args.length; i++) {
-			System.out.println(args[i]);
-		}
-	}
-}
