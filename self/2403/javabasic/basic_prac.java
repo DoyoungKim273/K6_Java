f7638d4651d43ce5aab7aaf04f4c13e02bab601d
Doyoung Kim <160606319+DoyoungKim273@users.noreply.github.com>
2024-03-14 17:24:34 +0900

    Update basic_prac.java

--- self/2403/javabasic/basic_prac.java
+++ self/2403/javabasic/basic_prac.java
@@ -10,24 +10,25 @@ public class List_prac02 {
 //		mydog.age = 52;
 //		System.out.println(mydog.breed + " " + mydog.age);
 //		
-		mydog.bark();
+		mydog.bark(); // 메서드 호출
 		
-		System.out.println(mydog.getBreed() + "  " + mydog.getAge());
+		System.out.println(mydog.getBreed() + "  " + mydog.getAge()); // getter 이용해서 값 출력
 		
-		String str = "hahaha";
+		String str = "hahaha"; // private 밖에서 breed 값과 age 값을 수정
 		mydog.setBreed(str);
 		int num = 0;
 		mydog.setAge(num);
 		
-		System.out.println(mydog.getBreed() + " " + mydog.getAge());
+		System.out.println(mydog.getBreed() + " " + mydog.getAge()); // 수정한 값 다시 출력
 		
-		Dog[] arr = new Dog [3];
+		Dog[] arr = new Dog [3]; 
 		arr[0] = new Dog("jindo1", 1);
 		arr[1] = new Dog("jindo2", 2);
 		arr[2] = new Dog("jindo3", 3);
 		
-//		Dog[] arr = {new Dog("jindo1", 1), new Dog("jindo2", 2), new Dog("jindo3", 3)};
-		System.out.println();
+//		Dog[] arr = {new Dog("jindo1", 1), new Dog("jindo2", 2), new Dog("jindo3", 3)}; -- 좀 더 짧게 배열 선언
+		
+		System.out.println(); // 바로 arr로 출력하면 안되고 당연히 for문 이용해서 인덱스 순회해서 출력해줘야함
 		for(int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i].toString()); // 문자열을 출력하기 위해서는 String toString 메서드를 써야함
 		}
@@ -35,7 +36,7 @@ public class List_prac02 {
 }
 
 class Dog{
-	private String breed; // 필드 선언
+	private String breed; // 필드 선언 -- private로 해서 getter, setter 연습
 	private int age;
 	
 	Dog(String breed, int age){ // 생성자
@@ -43,7 +44,7 @@ class Dog{
 		this.age = age;
 	}
 	
-	void bark() { // 메소
+	void bark() { // 메소드 선언
 		System.out.println();
 		System.out.println("Woof");
 	}
@@ -64,7 +65,7 @@ class Dog{
 		this.age = age;
 	}
 	
-	public String toString() {
+	public String toString() { // 배열이 문자열과 숫자로 구성되어 있으니 문자열로 반환해주는 메소드 써야함(아니면 주소값을 반환함)
 		return breed + " " + age;
 	}
 
