aeaa449dc85847b23f87ab01efaab503a2b06e86
Doyoung Kim <doctor2823@gmail.com>
2024-03-16 18:28:04 +0900

    overriding

--- self/2403/javabasic/Ch07/Imessenger.java
+++ self/2403/javabasic/Ch07/Imessenger.java
@@ -1,6 +1,8 @@
 package com.ruby.java.jvbasicCh07;
 
 public class Imessenger implements Messenger {
+// 인터페이스를 상속하는 클래스는 인터페이스의 본문을 갖는 default, static, private 메서드에 대해서는 오버라이딩 할 필요 X
+// 본문을 구현하지 않은 추상 메서드에 대해서는 반드시 오버라이딩, 본문을 구현해야함
 	
 	public String getMessage() {
 		return "Iphone";
